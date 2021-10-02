9a183b71b4a7c4494c11801dd3b345c84f90bd1c
Quraan App Project <quraanappproject@Quraans-MacBook-Pro.local>
2021-10-02 13:44:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "d276ca7038f24248a56c8c67e354d016"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
